3c7f8ec4b92abf5335d19bdab6037bd6438423d5
OrwellFlag <orwellflag@gmail.com>
2025-06-23 15:05:33 +0200

    igen

---
+++ wp.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo update
+helm upgrade --install --create-namespace wordpress-release bitnami/wordpress -n wordpress -f values.yaml
+echo kesz
