77c868468382e5711408485a00adeb64446ac3de
OrwellFlag <orwellflag@gmail.com>
2025-06-23 16:19:46 +0200

    cluster connect

--- wp.sh
+++ wp.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+gcloud container clusters get-credentials intern-cluster --region us-east1 --project intern-461913
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 helm upgrade --install --create-namespace wordpress-release bitnami/wordpress -n wordpress -f values.yaml
