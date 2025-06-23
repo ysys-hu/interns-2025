01efe793c27dabdda870ccb9c01bd1e80003cf0b
OrwellFlag <orwellflag@gmail.com>
2025-06-23 17:01:28 +0200

    -cret and -auth

--- wp.sh
+++ wp.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-gcloud components install gke-gcloud-auth-plugin
-gcloud container clusters get-credentials intern-cluster --region us-east1 --project intern-461913
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 helm upgrade --install --create-namespace wordpress-release bitnami/wordpress -n wordpress -f values.yaml
-echo kesz
